a439918fa89d06730ef37af2d11b0626dcbe310f
Roger Lo <sobadrush@icloud.com>
2024-12-28 20:46:28 +0800

    feat(all): init commit
    建立玩專案，第一次 commit
    
    Author: Roger Lo

---
+++ src/test/java/com/roger/springkafkatest/SpringKafkaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roger.springkafkatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
