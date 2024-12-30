9035a4e5132100e9fbff9a3733b635f9c4c586e9
Roger Lo <sobadrush@icloud.com>
2024-12-30 15:54:20 +0800

    fix(SpringKafkaTestApplicationTests): 復原 test case
    Author: Roger Lo

--- src/test/java/com/roger/springkafkatest/SpringKafkaTestApplicationTests.java
+++ src/test/java/com/roger/springkafkatest/SpringKafkaTestApplicationTests.java
@@ -1,24 +1,14 @@
 package com.roger.springkafkatest;
 
-import com.roger.springkafkatest.kafka.service.MessageConsumerService;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringKafkaTestApplicationTests {
 
-    @Autowired
-    private MessageConsumerService messageConsumerService;
-
     @Test
-    @DisplayName("[Test_001] 取得消費 Kafka 的紀錄")
-    // @Disabled
-    void test_001(TestInfo testInfo) {
-        System.out.println(" === " + testInfo.getDisplayName() + " === " );
-        System.out.println(messageConsumerService.getConsumedPartitions());
+    void contextLoad() {
+        System.out.println("SpringKafkaTestApplicationTests.contextLoad");
     }
 
 }
